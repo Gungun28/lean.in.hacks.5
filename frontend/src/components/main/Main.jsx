34155ff419cb52a97b5400aeea96d16798901137
Tanvii26 <tanviiverma26@gmail.com>
2024-03-19 14:17:59 +0530

    link update

--- frontend/src/components/main/Main.jsx
+++ frontend/src/components/main/Main.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
-import {Link} from "react-router-dom"
-import './Main.scss'
+// import React from 'react'
+// import {Link} from "react-router-dom"
+// import './Main.scss'
+// const Main = () => {
+//   return (
+//     <div className='main'>
+//       <div className="first">
+//         <Link to="/jewellery"><span>JEWELLERY</span></Link>
+//       </div>
+//       <div className="sec">
+//       <Link to="/phonecase"><span>PHONE CASE</span></Link>
+//       </div>
+//       <div className="last">
+//       <Link to="/purse"><span>PURSES</span></Link>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Main
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Main.scss';
+
 const Main = () => {
   return (
     <div className='main'>
-      <div className="first">
-        <Link to="/jewellery"><span>JEWELLERY</span></Link>
-      </div>
-      <div className="sec">
-      <Link to="/phonecase"><span>PHONE CASE</span></Link>
-      </div>
-      <div className="last">
-      <Link to="/purse"><span>PURSES</span></Link>
-      </div>
+      <Link to="/jewellery" className="link-first">
+        <div className="first">
+          <span>JEWELLERY</span>
+        </div>
+      </Link>
+      <Link to="/phonecase" className="link-sec">
+        <div className="sec">
+          <span>PHONE CASE</span>
+        </div>
+      </Link>
+      <Link to="/purse" className="link-last">
+        <div className="last">
+          <span>PURSES</span>
+        </div>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
